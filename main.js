1cc6caf24f0a623645d0ea53a2928ad039def141
eun ji <4484jjj@naver.com>
2023-03-21 22:40:19 +0900

    certificate 수정해야함

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ window.onload = () =>{
     const about = document.querySelector("#about > div")
     const tech = document.querySelector("#skills > div")
     const projects = document.querySelector("#projects > div")
-    const awards = document.querySelector("#awards > div") 
+    const certificate = document.querySelector("#certificate > div") 
     const add_class_on_scroll = (dom) => dom.classList.add("come-in")  
     window.addEventListener('scroll', function() { 
       scrollpos = window.scrollY; 
-      if (scrollpos >= about.offsetHeight -200) add_class_on_scroll(about)
-      if (scrollpos >= tech.offsetHeight -1300) add_class_on_scroll(tech)
-      if (scrollpos >= projects.offsetHeight +1600) add_class_on_scroll(projects)
-      if (scrollpos >= awards.offsetHeight +1200 ) add_class_on_scroll(awards) 
+      if (scrollpos >= about.offsetHeight -500) add_class_on_scroll(about)
+      if (scrollpos >= tech.offsetHeight -1200) add_class_on_scroll(tech)
+      if (scrollpos >= projects.offsetHeight +1200) add_class_on_scroll(projects)
+      if (scrollpos >= certificate.offsetHeight -1000 ) add_class_on_scroll(certificate) 
     }) 
 } 
 document.addEventListener("DOMContentLoaded", _ => {
